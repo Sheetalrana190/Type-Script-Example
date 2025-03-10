e847aa081cedded89ff0092d3aea7cd30272c6c2
sheetal <srana4226@conestoga.on.ca>
2025-03-10 17:14:47 -0400

    adding a test to calculate volume

--- volume/volume.test.ts
+++ volume/volume.test.ts
@@ -1,2 +1,8 @@
 //goal: Calculate the volume of prisim
 // Vollume = width * length * hight
+
+//30 = 2  * 3 * 5
+//volume is 30 when width = 2, length =3 and hight =5
+
+test("calculate the volume of a prisim", () => {
+});
